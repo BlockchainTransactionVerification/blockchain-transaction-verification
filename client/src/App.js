8327d8fc355d960528137a4901baec805e677491
jgwynn <46574659+jgwynn@users.noreply.github.com>
2021-09-19 14:35:34 -0400

    client changes
    dont really know what happened to cause these but they are irrelevant.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,11 @@
-
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
+import React from "react";
 
 const App = () => {
-  const [users, setUsers] = useState([])
-  const getData = async() => {
-    const res = await axios.get('/api/users')
-    setUsers(res.data)
-  }
-
-  useEffect(() => {
-    getData()
-  }, [])
- 
   return (
     <div>
-      {users.map(u => <h4 key={u._id}>userName : {u.userName}</h4>)}
+      <h1>App</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
