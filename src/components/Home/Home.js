c22c0f2b952ade94f4718374ae61ff3b1883c821
varunn1630 <varunchaudhari16@gmail.com>
2021-10-01 18:27:26 -0400

    Responsive buttons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -30,6 +30,7 @@ function Dashboard(props) {
 
   <li role="presentation" class="btn"><a href="#">Profile</a></li>
   {/*<li role="presentation" class="btn"><a href="#">Messages</a></li>-->*/}
+   
   <button type="button" class="btn primary">
   Home <span class="badge bg-primary"></span>
   <button type="button" class="btn primary">
@@ -37,11 +38,18 @@ function Dashboard(props) {
   <button type="button" class="btn primary">
   Messages <span class="badge bg-primary">7</span>
   <button type="button" class="btn primary">
-  Notifications <span class = "badge bg-primary">12</span>
+  Notifications <span class = "badge bg-primary"></span>
+  {/*<button onclick="location.href='page2.html'">
+  Add transactions*/}
+
+</button>
+</button>
+
+
   </button>
   </button>
-  </button>
-  </button>
+  
+  
 </ul>
 
 <div class="input-group">
@@ -50,30 +58,39 @@ function Dashboard(props) {
   <button type="button" class="btn btn-outline-primary">search</button>
 </div>
               
-  {/*<div className="card-body">
-    <h5 className="card-title">Active Transactions</h5>
-    <p className="card-text">
-    <ul className="list-group">
-  <li className="list-group-item">An item</li>
-  <li className="list-group-item">A second item</li>
-  <li className="list-group-item">A third item</li>
-  <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth one</li>*/}
 
-  <div class="card text-center">
+
+  {/*<div class="card text-center">
   <div class="card-header">
     <ul class="nav nav-tabs card-header-tabs">
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="true" href="#">Pending</a>
+        
+      </li>
       <li class="nav-item">
-        <a class="nav-link active" aria-current="true" href="#">Active</a>
+        <a class="nav-link" href= "#">Active</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Inactive</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled">Disabled</a>
-      </li>
     </ul>
-  </div>
+</div>*/}
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Pending</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Active</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Inactive</button>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+</div>
   <div class="card-body">
     <h5 class="card-title">Active Transactions</h5>
     <p className="card-text">
@@ -95,7 +112,7 @@ function Dashboard(props) {
     </div>
   </div>
 </div>
-        </div>
+        
 
     )
 }
