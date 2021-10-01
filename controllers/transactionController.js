23f8dbcdc5488416286d99318b8930ff2cc34e4d
jgwynn <46574659+jgwynn@users.noreply.github.com>
2021-10-01 17:48:08 -0400

    Update transactionController.js

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,4 +1,5 @@
-import User from '../models/usersModel.js'
+import Transaction from '../models/transactionModel.js'
+import User from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
@@ -10,89 +11,86 @@ import sgMail from '@sendgrid/mail'
         // success - status: 200; success: true, msg: "User has been successfully activated"
         // failed - status: 442; error:"some message"
 
-export const registerUser = asyncHandler(async(req, res) => {
-    console.log("you are in register api")
-    const {Username, Password, Email, CompanyName, BusinessAddress, 
-           RepFirstName, RepLastName, Position} = req.body
-    if(!Username || !Password || !Email){
+export const addTransaction = asyncHandler(async(req, res) => {
+    console.log("you are in addTransaction")
+    const {BuyerID, SellerID} = req.body
+    if(!BuyerID || !SellerID){
         return res.status(442).json({error:"please add all the fields"})
     }
-    //checks database for a user with this username
-    User.findOne({Username:Username})
-    .then(savedUser=>{
-        //if a user by this username exists, error
-        if(savedUser){
-            return res.status(442).json({error:"Username Taken"})
-        }
-        //if it does not exist, hash the password
-       bcrypt.hash(Password,12)
-         .then(hashedPassword=>{
-             const Users = new User({
-                 Username,
-                 Password:hashedPassword,
-                 Email,
-                 CompanyName,
-                 BusinessAddress,
-                 RepFirstName,
-                 RepLastName,
-                 Position,
-                 // temporarytoken: jwt.sign(Username, JWT_SECRET)
-                 temporarytoken: jwt.sign(Username, process.env.JWT_SECRET),
-                 active: false
-             })
-             Users.save()
-             .then(user=>{
-                 console.log("saved successfully")
-                 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-                 const hrefLink = "https://blkchn-trxn-verif.herokuapp.com/api/verify/" + Users.temporarytoken;
-                 const msg = {
-                     to: user.Email, // Change to your recipient
-                     from: 'BlockChainUCFSD@gmail.com', // Change to your verified sender
-                     subject: 'Thank you For Registering with BlockChain Transaction Verification',
-                     text: `Hello ${Users.Username}, Click Here to Activate your Account.`,
-                     //html: `Hello<strong> ${Users.FirstName}</strong>,<br><br> Click Here to Activate your Account or don't I am not your mom`,
-                     html: `Hello<strong> ${Users.Username}</strong>,<br><br><a href=${hrefLink}> Click Here to Activate your Account.</a>`,
-                 }
-                 sgMail.send(msg)
-                 .then(() => {
-                     console.log('Email sent')
-                 })
-                 .catch((error) => {
-                     console.error(error)
-                 })
-                 res.json({
-                     //ID: user.id,
-                     success: true,
-                     msg: "User has been successfully activated"
-                 });
-              })
-             .catch(err=>{console.log(err)})
-         }).catch(err=>{console.log(err)})
+    
+    const transaction =new Transaction({
+          BuyerID,
+          SellerID
     })
+    const buyer = await User.findById(BuyerID)
+    const seller = await User.findById(SellerID)
+    console.log(buyer)
+    console.log(seller)
+    if(!buyer || !seller){
+        return res.status(442).json({error:"User not found"})
+    }
+    
+    updates.save()
+    .then((result, err) => {
+        if(err){
+            return res.status(442).json({error: err})
+        }
+    })
+        
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const hrefLink = "https://blkchn-trxn-verif.herokuapp.com/;
+    const msg = {
+        to: seller.Email, // Change to your recipient
+        from: 'BlockChainUCFSD@gmail.com', // Change to your verified sender
+        subject: 'New Pending Transaction',
+        text: `Hello ${seller.RepFirstName}, You have a new Pending Transaction`,
+        //html: `Hello<strong> ${Users.FirstName}</strong>,<br><br> Click Here`,
+        html: `Hello<strong> ${Users.Username}</strong>,<br><br><a href=${hrefLink}> Click Here </a> and Log in to check out 
+               your new pending transaction from ${buyer.RepFirstName} from Company Stand in`,
+    }
+    sgMail.send(msg)
+    .then(() => {
+        console.log('Email sent')
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+     res.json({
+         //ID: user.id,
+         success: true,
+         msg: "User has been successfully activated"
+     })
 })
 
-//update transaction
-//input - id and any other valid field for users
+//update documents
+//input - id and Documents
 //output -
         // success - status: 200; success: true, msg: "User has been successfully edited"
         // failed - status: 442; error:"some message"
 
-export const updateUser = asyncHandler(async(req, res) => {
-  if(!req.body.id){
-      return res.status(442).json({error:"Transaction ID is missing"})
-  }
-  if(req.body.Documents){
-    //to be added
-  }
-  const updates = await User.findById(req.body.id)
-  if(!updates){
-        return res.status(442).json({error:"Transaction not found"})
-  }
-  
-  
-  
-  
-  
+export const updateDocuments = asyncHandler(async(req, res) => {
+    if(!req.body.id){
+          return res.status(442).json({error:"Transaction ID is missing"})
+      }
+      if(!req.body.Documents){
+        //to be added
+      }
+      const updates = await User.findById(req.body.id)
+      if(!updates){
+            return res.status(442).json({error:"Transaction not found"})
+      }
+      updates[Documents] = req.body[Documents]
+      updates.save()
+      .then((result, err) => {
+          if(err){
+              return res.status(442).json({error: err})
+          }else{
+              return res.json({
+                  success: true,
+                  msg: "User has been successfully edited"
+              })
+          }
+      })
   
 
 })
