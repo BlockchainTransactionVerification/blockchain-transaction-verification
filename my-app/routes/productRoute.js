b117b666d327cf9705835492efd35e3563266df3
varunn1630 <54746172+varunn1630@users.noreply.github.com>
2021-10-30 00:22:07 -0500

    lot of updates

---
+++ my-app/routes/productRoute.js
@@ -0,0 +1,20 @@
+import { saveItem, getItems} from "../controllers/productController.js";
+import express from 'express'
+import jwt from "jsonwebtoken";
+import Supply from '../models/productModel.js'
+const productRouter = express.Router()
+import auth from '../auth/auth.js'
+
+
+
+// express router method to create route for uploading information on an item to be sold
+productRouter.route('/addItem').post(auth, saveItem)
+
+// express router method to create route for logging in users
+productRouter.route('/getItem').get(getItems)
+
+// express router method to create route for deleting users
+//productRouter.route('/delete/:id').delete(deleteUser)
+
+
+export default productRouter
