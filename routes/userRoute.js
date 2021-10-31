3351fcc0cb651ddf0270d5bd869e4c4bf43d438a
Jstevens-Dev <jas.traveler.95@gmail.com>
2021-10-31 12:11:29 -0400

    Register page axios call is working.

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,24 +1,27 @@
-import { registerUser, loginUser, deleteUser, verifyUser, updateUser} from "../controllers/userController.js";
-import express from 'express'
-
-import User from '../models/usersModel.js'
-const router = express.Router()
-
-
+import {
+  registerUser,
+  loginUser,
+  deleteUser,
+  verifyUser,
+  updateUser,
+} from "../controllers/userController.js";
+import express from "express";
+import User from "../models/usersModel.js";
+const router = express.Router();
 
 // express router method to create route for registering users
-router.route('/register').post(registerUser)
+router.route("/register").post(registerUser);
 
 // express router method to create route for logging in users
-router.route('/login').post(loginUser)
+router.route("/login").post(loginUser);
 
 // express router method to create route for deleting users
-router.route('/delete/:id').delete(deleteUser)
+router.route("/delete/:id").delete(deleteUser);
 
 // router method to create route for verifying email
-router.route('/verify/:id').put(verifyUser)
+router.route("/verify/:id").put(verifyUser);
 
 // router method to create route for updating user information
-router.route('/update').put(updateUser)
+router.route("/update").put(updateUser);
 
-export default router
+export default router;
--- server.js
+++ server.js
@@ -1,39 +1,42 @@
-import connectDB from './config/db.js'
-import userRoutes from './routes/userRoute.js'
-import productRoutes from './routes/productRoute.js'
-import transactionRoutes from './routes/transactionRoute.js'
-import express from 'express'
-import dotenv  from 'dotenv'
-import cors from 'cors'
-import path from 'path'
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoute.js";
+import productRoutes from "./routes/productRoute.js";
+import transactionRoutes from "./routes/transactionRoute.js";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
 
-const PORT = 3000
-//process.env.PORT || 3000
-//connect database
-connectDB()
+const PORT = 5000;
+
+connectDB();
 
 //dotenv config
-dotenv.config()
+dotenv.config();
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+
+app.options("*", cors());
 
 //Creating API for user
-app.use('/api', userRoutes)
-app.use('/apisup', productRoutes)
-app.use('/apitra', transactionRoutes)
+app.use("/api", userRoutes);
+app.use("/apisup", productRoutes);
+app.use("/apitra", transactionRoutes);
 
-
-app.use(cors())
+app.use(cors);
 
 //for getting heroku to work
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-    
-    app.get("*", (req, res) => {
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
+  });
 }
 
 //Express js listen method to run project on http://localhost:5000
-app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
