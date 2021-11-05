a0ab04eaaeb4072971b98febf3db7464a86a24ad
jgwynn <46574659+jgwynn@users.noreply.github.com>
2021-11-04 23:44:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,15 +17,12 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors);
 app.options("*", cors());
 
 //for getting heroku to work
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
 }
 
 //Creating API for user
@@ -34,7 +31,9 @@ app.use("/apisup", productRoutes);
 app.use("/apitra", transactionRoutes);
 app.use("/apisop", sopRouter);
 
-app.use(cors);
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 //Express js listen method to run project on http://localhost:PORT
 app.listen(
