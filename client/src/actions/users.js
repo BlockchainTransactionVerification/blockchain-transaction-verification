1a153734f3ba974cfc3bd89f1397af23f5ed2f89
jgwynn <46574659+jgwynn@users.noreply.github.com>
2021-11-04 23:58:37 -0400

    Update users.js

--- client/src/actions/users.js
+++ client/src/actions/users.js
@@ -18,7 +18,7 @@ export const login = (username, password) => async (dispatch) => {
 
     const { data } = await axios.post(
       //"http://localhost:5000/api/login",
-      "https://blkchn-trxn-verif.herokuapp.com/",
+      "https://blkchn-trxn-verif.herokuapp.com/login",
       { Username: username, Password: password },
       config
     );
