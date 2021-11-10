d0e4114ecefef9b2db19d9830c5a0f8b81ede2e7
Hannah Main <mainhannah@knights.ucf.edu>
2021-11-10 16:11:56 -0500

    Revert "Add files via upload"
    This reverts commit e5155958c3734c937c0d4961c5cabe56c2025433.

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -1,35 +1,45 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const transactionSchema = mongoose.Schema({
-    ProdID:{
-		type:String,
-		required:true
-	},
-	BuyerID:{
-		type:String,
-		required:true
-	},
-	SellerID:{
-		type:String,
-		required:true
-	},
-	Active: {
-		type: Boolean,
-		default: false,
-		required: true
-	},
-	Pending: {
-		type: Boolean,
-		default: true,
-		required: true
-	},
-	Documents: [{
-		DocName: String,
-		Responsible: String,
-		Done: Boolean
-	}]
-})
+  ProdID: {
+    type: String,
+    required: true,
+  },
+  BuyerID: {
+    type: String,
+    required: true,
+  },
+  SellerID: {
+    type: String,
+    required: true,
+  },
+  Active: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+  Pending: {
+    type: Boolean,
+    default: true,
+    required: true,
+  },
+  TransactionURL: {
+    type: String,
+    default: "",
+  },
+  Title: {
+    type: String,
+    default: "",
+  },
+  Documents: [
+    {
+      DocName: String,
+      Responsible: String,
+      Done: Boolean,
+    },
+  ],
+});
 
-const Transaction = mongoose.model('Transactions', transactionSchema)
+const Transaction = mongoose.model("Transactions", transactionSchema);
 
-export default Transaction
+export default Transaction;
--- models/usersModel.js
+++ models/usersModel.js
@@ -1,90 +1,51 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-    Username:{
-		type:String,
-		required:true
-	},
-	Password:{
-		type:String,
-		required:true
-	},
-    Email:{
-		type:String,
-		required:true
-	},
-	CompanyName:{
-		type:String,
-	},
-	BusinessAddress:{
-		type:String,
-	},
-    RepFirstName:{
-		type:String,
-	},
-	RepLastName: {
-		type: String,
-	},
-	Position: {
-		type: String,
-	},
-	WalletID:{
-		type:String,
-	},
-	temporarytoken: {
-		type: String,
-		required: true
-	},
-	active: {
-		type: Boolean,
-		required: true,
-		default: false
-	},
-	isSeller: {
-		type: Boolean,
-		required: true
-	}
-})
+  Username: {
+    type: String,
+    required: true,
+  },
+  Password: {
+    type: String,
+    required: true,
+  },
+  Email: {
+    type: String,
+    required: true,
+  },
+  CompanyName: {
+    type: String,
+  },
+  BusinessAddress: {
+    type: String,
+  },
+  RepFirstName: {
+    type: String,
+  },
+  RepLastName: {
+    type: String,
+  },
+  Position: {
+    type: String,
+  },
+  isSeller: {
+    type: Boolean,
+    required: true,
+  },
+  WalletID: {
+    type: String,
+  },
+  temporarytoken: {
+    type: String,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
 
-/**
- * @param {String} id, user id
- * @return {Object} User profile object
- */
- userSchema.statics.getUserById = async function (id) {
-	try {
-	  const user = await this.findOne({ _id: id });
-	  if (!user) throw ({ error: 'No user with this id found' });
-	  return user;
-	} catch (error) {
-	  throw error;
-	}
-  }
+const User = mongoose.model("User", userSchema);
 
-   /**
- * @return {Array} List of all users
- */
-userSchema.statics.getUsers = async function () {
-	try {
-	  const users = await this.find();
-	  return users;
-	} catch (error) {
-	  throw error;
-	}
-  }
-  
-  /**
-   * @param {Array} ids, string of user ids
-   * @return {Array of Objects} users list
-   */
-  userSchema.statics.getUserByIds = async function (ids) {
-	try {
-	  const users = await this.find({ _id: { $in: ids } });
-	  return users;
-	} catch (error) {
-	  throw error;
-	}
-  }
-
-const User = mongoose.model('User', userSchema)
-
-export default User
+export default User;
