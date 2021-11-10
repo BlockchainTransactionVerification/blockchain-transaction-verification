db660c9cb10a0e9db792d22a4f7fffe06d788f33
Hannah Main <mainhannah@knights.ucf.edu>
2021-11-10 16:10:40 -0500

    Revert "Add files via upload"
    This reverts commit 5fe164b0b61e5012c8d591b0365176bd1aa81323.

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -1,20 +1,29 @@
-import { addTransaction, getTransaction, getDocuments, updateDocuments } from "../controllers/transactionController.js";
-import express from 'express'
+import {
+  addTransaction,
+  getTransaction,
+  getDocuments,
+  updateDocuments,
+  updateTransactionStatus,
+} from "../controllers/transactionController.js";
+import express from "express";
 
-import transaction from '../models/transactionModel.js'
-const transactionRouter = express.Router()
+import transaction from "../models/transactionModel.js";
+const transactionRouter = express.Router();
 
+// express router method to create route to create a transaction
+transactionRouter.route("/addTransaction").post(addTransaction);
 
+// express router method to create route for updating transaction documents
+transactionRouter.route("/updateDocuments").post(updateDocuments);
 
-// express router method to create route for uploading information on an item to be sold
-transactionRouter.route('/addTransaction').post(addTransaction)
+// express router method to create route for getting transactions documents
+transactionRouter.route("/getDocuments").post(getDocuments);
 
-// express router method to create route for logging in users
-transactionRouter.route('/updateDocuments').post(updateDocuments)
+transactionRouter.route("/getTransaction").post(getTransaction);
 
-// express router method to create route for deleting users
-transactionRouter.route('/getDocuments').get(getDocuments)
+// express router method to create a route to update a transaction from pending to active, or from active to complete
+transactionRouter
+  .route("updateTransactionStatus")
+  .post(updateTransactionStatus);
 
-transactionRouter.route('/getTransaction').get(getTransaction)
-
-export default transactionRouter
+export default transactionRouter;
