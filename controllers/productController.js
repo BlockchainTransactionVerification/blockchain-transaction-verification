c278384fda369fda88829070518cb7ad44fc9b92
Hannah Main <mainhannah@knights.ucf.edu>
2021-11-10 16:11:50 -0500

    Revert "Add files via upload"
    This reverts commit fee00ff695a3715fc995c1ed3d42beeac2469d94.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,80 +1,92 @@
-import Supply from "../models/productModel.js";
-import asyncHandler from "express-async-handler";
+import Supply from '../models/productModel.js'
+import asyncHandler from 'express-async-handler'
 
-export const saveItem = asyncHandler(async(req, res) => {
-    console.log("you are in saveItem api");
-    const {
-        SellerID,
-        ItemName,
-        Quantity,
-        Quality,
-        Price,
-        Brand,
-        NDA,
-        Region,
-        ProdRate,
-        ShipRestrict
-    } = req.body;
-    if (!SellerID || !ItemName) {
-        return res.status(442).json({ error: "please add all the fields" });
-    }
-    // to be made required. Taken out for ease of testing.
-    //|| !Quantity || !Quality || !LevelSafety || !Region || !NDA
-    const Supplies = new Supply({
-        SellerID,
-        ItemName,
-        Quantity,
-        Quality,
-        Price,
-        Brand,
-        NDA,
-        Region,
-        ProdRate,
-        ShipRestrict
-    });
-    Supplies.save()
-        .then(() => {
-            console.log("save successful");
-            res.json({
-                success: true,
-                msg: "This Item has been saved Successfully",
-            });
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-});
+export const addProduct = asyncHandler(async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  console.log('you are in saveItem api')
+  const {
+    SellerID,
+    ItemName,
+    Quantity,
+    Quality,
+    Price,
+    Brand,
+    Region,
+    ProdRate,
+    ShipRestrict,
+  } = req.body
+  if (!SellerID || !ItemName) {
+    return res.status(442).json({ error: 'please add all the fields' })
+  }
+  // to be made required. Taken out for ease of testing.
+  //|| !Quantity || !Quality || !LevelSafety || !Region || !NDA
+  const Supplies = new Supply({
+    SellerID,
+    ItemName,
+    Quantity,
+    Quality,
+    Price,
+    Brand,
+    Region,
+    ProdRate,
+    ShipRestrict,
+  })
+  Supplies.save()
+    .then(() => {
+      console.log('save successful')
+      res.json({
+        success: true,
+        msg: 'This Item has been saved Successfully',
+      })
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+})
 
 //input: ItemName, Quantity, Price, supplier
 
 export const getItems = asyncHandler(async (req, res) => {
-    //if(req.body.supplier)
-    //send back all
-    //else
-    //return limited
+  //if(req.body.supplier)
+  //send back all
+  //else
+  //return limited
+  //res.set('Access-Control-Allow-Origin', '*')
+  const { ItemName, Quantity, Price, supplier } = req.body
 
-    const {ItemName, Quantity, Price, isSeller} = req.body;
-    if (!ItemName || !Quantity || !Price || (isSeller == null)) {
-        return res.status(442).json({ error: "please add all the fields" });
-    }
+  if (!ItemName || !Quantity || !Price ) {
+    return res.status(442).json({ error: 'please add all the fields' })
+  }
 
-
-    if(isSeller){
-        Supply.find({ItemName: req.body.ItemName, 
-                     Quantity: {$lte: req.body.Quantity}, 
-                     Price: {$lte: req.body.Price},
-                     Brand: req.body.Brand,
-                     isOnGround: req.body.isOnGround})
-        .then(savedItems =>{
-            return res.json(savedItems);
-        }).catch(err=>{console.log(err)})
-    } else{
-        //return limited fields
-        Supply.find({ItemName: req.body.ItemName, 
-            Quantity: {$lte: req.body.Quantity}, 
-            Price: {$lte: req.body.Price}}, 'ItemName Quantity Price')
-        .then(savedItems =>{
-            return res.json(savedItems);
-        }).catch(err=>{console.log(err)})
-    }
-});
+  if (req.body.supplier) {
+    Supply.find({
+      ItemName: req.body.ItemName,
+      Quantity: { $lte: req.body.Quantity },
+      Price: { $lte: req.body.Price },
+      Brand: req.body.Brand,
+      isOnGround: req.body.isOnGround,
+    })
+      .then((savedItems) => {
+        return res.json(savedItems)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  } else {
+    //return limited fields
+    Supply.find(
+      {
+        ItemName: req.body.ItemName,
+        Quantity: { $lte: req.body.Quantity },
+        Price: { $lte: req.body.Price },
+      },
+      'ItemName Quantity Price'
+    )
+      .then((savedItems) => {
+        return res.json(savedItems)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+})
