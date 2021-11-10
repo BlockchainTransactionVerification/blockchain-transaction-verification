5c2a7c9e9f44434a964052e7bcd84d3b863722fe
iswimming <34221565+iswimming@users.noreply.github.com>
2021-11-10 15:14:41 -0500

    Add files via upload

---
+++ userRoute.js
@@ -0,0 +1,39 @@
+import { registerUser, loginUser, deleteUser, verifyUser, updateUser, registerUserMobile, verifyUserMobile, passResetEmail, ResetPassword} from "../controllers/userController.js";
+import express from 'express'
+
+import User from '../models/usersModel.js'
+const router = express.Router()
+
+
+
+// express router method to create route for registering users
+router.route('/register').post(registerUser)
+
+// express router method to create route for logging in users
+router.route('/login').post(loginUser)
+
+// express router method to create route for deleting users
+router.route('/delete/:id').delete(deleteUser)
+
+// router method to create route for verifying email
+router.route('/verify/:id').put(verifyUser)
+
+// router method to create route for updating user information
+router.route('/update').put(updateUser)
+
+// router method to create route for updating user information
+router.route('/registerMobile').post(registerUserMobile)
+
+// router method to create route for updating user information
+router.route('/verifyMobile').put(verifyUserMobile)
+
+// router method to create route for updating user information
+router.route('/passResetEmail').post(passResetEmail)
+
+// router method to create route for updating user information
+router.route('/ResetPassword/:id').put(ResetPassword)
+
+router.route('/'). get(onGetAllUsers)
+router.route('/:_id'). get(onGetUserById)
+
+export default router
