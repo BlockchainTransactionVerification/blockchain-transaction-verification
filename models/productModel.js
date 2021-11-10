e5155958c3734c937c0d4961c5cabe56c2025433
iswimming <34221565+iswimming@users.noreply.github.com>
2021-11-10 15:38:39 -0500

    Add files via upload

--- models/productModel.js
+++ models/productModel.js
@@ -1,51 +1,52 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
 const supplySchema = mongoose.Schema({
-  ItemName: {
-    type: String,
-    required: true,
-  },
-  Quantity: {
-    type: Number,
-    required: true,
-  },
-  Quality: {
-    type: String,
-    required: true,
-  },
-  Price: {
-    type: String,
-    required: true,
-  },
-  Brand: {
-    type: String,
-    required: true,
-  },
-  NDA: {
-    type: Boolean,
-  },
-  SellerID: {
-    type: String,
-    required: true,
-  },
-  Region: {
-    type: String,
-    required: true,
-  },
-  ProdRate: {
-    type: String,
-    required: true,
-  },
-  ShipRestrict: {
-    type: String,
-    required: true,
-  },
-  isOnGround: {
-    type: Boolean,
-    required: false,
-  },
-});
+    ItemName:{
+		type:String,
+		required:true
+	},
+	Quantity:{
+		type:Number,
+		required:true
+	},
+	Quality:{
+		type:String,
+		required:true
+	},
+	Price: {
+		type: String,
+		required: true
+	},
+	Brand: {
+		type: String,
+		required: true
+	},
+	NDA: {
+		type: Boolean,
+		required: true
+	},
+	SellerID: {
+		type: String,
+		required: true
+	},
+	Region: {
+		type: String,
+		required: true
+	},
+	ProdRate: {
+		type: String,
+		required: true
+	},
+    ShipRestrict: {
+		type: String,
+		required: true
+	},
+	isOnGround: {
+		type: Boolean,
+		required: false
+	}
+})
 
-const Supply = mongoose.model("Supplys", supplySchema);
+const Supply = mongoose.model('Supplys', supplySchema)
 
-export default Supply;
+export default Supply
