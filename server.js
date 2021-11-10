721b711bfc60b47aad6d1b34cd5b0b59d136ee6d
Hannah Main <mainhannah@knights.ucf.edu>
2021-11-10 16:10:16 -0500

    Revert "updated for chat funct"
    This reverts commit 04b40bfc9919133141cfef9c7ce04a8c96e00c74.

--- server.js
+++ server.js
@@ -1,69 +1,52 @@
-import http from "http"
-import logger from "morgan"
-import socketio from 'socket.io'
-import connectDB from './config/db.js'
-import userRoutes from './routes/userRoute.js'
-import productRoutes from './routes/productRoute.js'
-import chatRoomRouter from './routes/chatroom.js'
-import indexRouter from './routes/index.js'
-import transactionRoutes from './routes/transactionRoute.js'
-import Websockets from './utils/WebSockets.js'
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoute.js";
+import productRoutes from "./routes/productRoute.js";
+import transactionRoutes from "./routes/transactionRoute.js";
+import sopRouter from "./routes/sopRoute.js";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
 
-import express from 'express'
-import dotenv  from 'dotenv'
-import cors from 'cors'
-import path from 'path'
+const PORT = process.env.PORT || 3000;
 
-
-const app = express();
-const PORT = process.env.PORT || "5000";
-app.set("PORT", port);
-
-//connect database
-connectDB()
+connectDB();
 
 //dotenv config
-dotenv.config()
+dotenv.config();
 
-
-app.use(logger("dev"));
+const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
+app.options("*", cors());
+const __dirname = path.resolve();
 //Creating API for user
-//app.options('*', cors()) // include before other routes
-app.use('/api', userRoutes);
-app.use('/apisup', productRoutes);
-app.use("/", indexRouter);
-app.use("/room", chatRoomRouter);
-app.use('/apitra', transactionRoutes);
 
-
-app.use(cors())
+app.use("/api", userRoutes);
+app.use("/apisup", productRoutes);
+app.use("/apitra", transactionRoutes);
+app.use("/apisop", sopRouter);
+/* app.get("*", (req, res) =>
+  res.redirect("https://blkchn-trxn-verif.herokuapp.com/")
+); */
+//app.get("*", (req, res) => res.redirect("http://localhost:3000"));
 
 //for getting heroku to work
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-    
-    app.get("*", (req, res) => {
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
 }
 
+app.use(cors);
 
-//Express js listen method to run project on http://localhost:5000
-    //app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
-
-//this should accomplish the same thing as above but also add the websockets
-
-/* Create HTTP server. */
-const server = http.createServer(app);
-/* Create socket connection */
-global.io = socketio.listen(server);
-global.io.on('connection', WebSockets.connection)
-/* Listen on provided port, on all network interfaces. */
-server.listen(PORT);
-/* Event listener for HTTP server "listening" event. */
-server.on("listening", () => {
+//Express js listen method to run project on http://localhost:PORT
+app.listen(
+  PORT,
   console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-});
+);
